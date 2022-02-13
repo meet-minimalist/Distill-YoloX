a32d0726d56bc2aaf3d68d0a5dfaeba3e774ed75
meet-minimalist <patelmeet2012@gmail.com>
2022-02-13 11:05:32 +0530

    Updated coco dataset loader file to accomodate val2017 and minitrain2017.json

--- command.sh
+++ command.sh
@@ -1,8 +1,11 @@
 # python tools/train_distill.py --student-name yolox-s_distill --teacher-name yolox-l_distill --teacher-ckpt ./pretrained_ckpt/yolox_l.pth -d 0 -b 12 -o --fp16
 
 # training with train set and drive checkpointing
-python tools/train_distill.py --student-name yolox-s_distill --teacher-name yolox-l_distill --teacher-ckpt ./pretrained_ckpt/yolox_l.pth -d 0 -b 12 -o --fp16 data_dir "./datasets/COCO17/" train_ann "instances_train2017.json" max_epoch 100 output_dir "/content/drive/MyDrive/ThesisData/YoloXCheckpoints/yolox_s_distill/"
+python tools/train_distill.py --student-name yolox-s_distill --teacher-name yolox-l_distill --teacher-ckpt ./pretrained_ckpt/yolox_l.pth -d 1 -b 12 -o --fp16 data_dir "./datasets/COCO17/" train_ann "instances_train2017.json" max_epoch 100 output_dir "/content/drive/MyDrive/ThesisData/YoloXCheckpoints/yolox_s_distill/"
 
 # training with valid set and drive checkpointing
 # python tools/train_distill.py --student-name yolox-s_distill --teacher-name yolox-l_distill --teacher-ckpt ./pretrained_ckpt/yolox_l.pth -d 0 -b 12 -o --fp16 data_dir "./datasets/COCO17/" train_ann "instances_val2017.json" max_epoch 100 output_dir "/content/drive/MyDrive/ThesisData/YoloXCheckpoints/yolox_s_distill/"
+
+# Train on minitrain-2017 Normal training
+python tools/train.py -n yolox-s -d 1 -b 32 --fp16 -o --fp16 data_dir "./datasets/COCO17/" train_ann "instances_minitrain2017.json" max_epoch 60 output_dir "/content/drive/MyDrive/ThesisData/YoloXCheckpoints/yolox_s_normal/"
 
--- setup_training.sh
+++ setup_training.sh
@@ -1,8 +1,8 @@
 
-
 pip install -r requirements.txt
 
 mkdir ./pretrained_ckpt
+
 wget -c https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_s.pth -O ./pretrained_ckpt/yolox_s.pth
 wget -c https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_m.pth -O ./pretrained_ckpt/yolox_m.pth
 wget -c https://github.com/Megvii-BaseDetection/YOLOX/releases/download/0.1.1rc0/yolox_l.pth -O ./pretrained_ckpt/yolox_l.pth
@@ -19,6 +19,7 @@ wget -c http://images.cocodataset.org/zips/train2017.zip
 wget -c http://images.cocodataset.org/zips/val2017.zip
 wget -c http://images.cocodataset.org/zips/test2017.zip
 wget -c http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lezhgY4M_Ag13w0dEzQ7x_zQ_w0ohjin' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lezhgY4M_Ag13w0dEzQ7x_zQ_w0ohjin" -O ./datasets/COCO17/annotations/instances_minitrain2017.json && rm -rf /tmp/cookies.txt
 
 unzip train2017.zip -d ./COCO17
 unzip val2017.zip -d ./COCO17
