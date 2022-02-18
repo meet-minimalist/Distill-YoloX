ec54227387cd3879748d0d85aef8cced57c3450f
meet-minimalist <patelmeet2012@gmail.com>
2022-02-18 10:18:07 +0530

    Brought back decode_outputs function

--- yolox/models/yolo_head_vanilla.py
+++ yolox/models/yolo_head_vanilla.py
@@ -190,3 +190,20 @@ class YOLOXHead(nn.Module):
             else:
                 return outputs
 
+
+    def decode_outputs(self, outputs, dtype):
+        grids = []
+        strides = []
+        for (hsize, wsize), stride in zip(self.hw, self.strides):
+            yv, xv = torch.meshgrid([torch.arange(hsize), torch.arange(wsize)])
+            grid = torch.stack((xv, yv), 2).view(1, -1, 2)
+            grids.append(grid)
+            shape = grid.shape[:2]
+            strides.append(torch.full((*shape, 1), stride))
+
+        grids = torch.cat(grids, dim=1).type(dtype)
+        strides = torch.cat(strides, dim=1).type(dtype)
+
+        outputs[..., :2] = (outputs[..., :2] + grids) * strides
+        outputs[..., 2:4] = torch.exp(outputs[..., 2:4]) * strides
+        return outputs
