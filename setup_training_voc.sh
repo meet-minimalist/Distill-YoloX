16eae5143931c2edaf3860aa57c0f6c00781f99b
meet-minimalist <patelmeet2012@gmail.com>
2022-02-25 10:37:49 +0530

    Updated voc dataset scripts.

--- setup_training_voc.sh
+++ setup_training_voc.sh
@@ -13,10 +13,10 @@ wget -c https://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
 wget -c https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
 wget -c http://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
 
-unzip VOC2012test.tar -d ./datasets/VOC0712
-unzip VOCtrainval_11-May-2012.tar -d ./datasets/VOC0712
-unzip VOCtrainval_06-Nov-2007.tar -d ./datasets/VOC0712
-unzip VOCtest_06-Nov-2007.tar -d ./datasets/VOC0712
+tar -xvf VOC2012test.tar -C ./datasets/VOC0712
+tar -xvf VOCtrainval_11-May-2012.tar -C ./datasets/VOC0712
+tar -xvf VOCtrainval_06-Nov-2007.tar -C ./datasets/VOC0712
+tar -xvf VOCtest_06-Nov-2007.tar -C ./datasets/VOC0712
 
 mv ./datasets/VOC0712/VOC2012test/VOCdevkit/VOC2012 ./datasets/VOC0712/VOC2012/
 mv ./datasets/VOC0712/VOCtrainval_11-May-2012/VOCdevkit/VOC2012 ./datasets/VOC0712/VOC2012/
